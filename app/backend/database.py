7c09164a471c33ad552e094b9af6a6df09fbba99
Yassine <hachmo17@stud.thga.de>
2026-08-02 12:27:30 +0200

    Containerize backend with Docker Compose, connect to db service

--- app/backend/database.py
+++ app/backend/database.py
@@ -10,7 +10,7 @@ DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_NAME = os.getenv("POSTGRES_DB")
 DB_PORT = os.getenv("POSTGRES_PORT")
-DB_HOST = "localhost"
+DB_HOST = os.getenv("DB_HOST", "localhost")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
